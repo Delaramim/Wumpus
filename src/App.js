14e37f1f362f62c3d5f53e212e0c99dc7090db54
Delara Mohammmadi <delara.mohammadi@stu.ssst.edu.ba>
2020-12-20 17:04:06 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Tile from "./Components/tile";
+import Tiles from "./Tiles";
 
 function App() {
   return (
     <div className="App">
       <div className="Container">
-        <Tile></Tile>
+        <Tiles></Tiles>
       </div>
     </div>
   );
